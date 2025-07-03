580d80fa8df4493d7f329d7852d0ee51ae6a1890
wirlleymarcos <wirlley17@gmail.com>
2025-07-03 16:31:27 -0300

    correction and upgrade version

--- android/src/main/java/br/net/mlsolutions/app/printthermal/PrintThermalPlugin.java
+++ android/src/main/java/br/net/mlsolutions/app/printthermal/PrintThermalPlugin.java
@@ -20,21 +20,18 @@ public class PrintThermalPlugin extends Plugin {
 
     @PluginMethod
     public void listPrinters(PluginCall call) {
-        @PluginMethod
-        public void listPrinters(PluginCall call) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.BLUETOOTH_CONNECT)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1001);
-                    call.reject("BLUETOOTH_CONNECT permission is required");
-                    return;
-                }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.BLUETOOTH_CONNECT)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1001);
+                call.reject("BLUETOOTH_CONNECT permission is required");
+                return;
             }
-
-            PrintThermal printer = new PrintThermal(getContext());
-            JSObject result = printer.listPrinters(call);
-            call.resolve(result);
         }
+
+        PrintThermal printer = new PrintThermal(getContext());
+        JSObject result = printer.listPrinters(call);
+        call.resolve(result);
     }
 
     @PluginMethod
